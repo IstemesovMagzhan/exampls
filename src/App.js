22a540b9ab1c1dd2c8cec06ccadc36ac46be4615
Magzhan Istemesov <istemesov.magzhan@mail.ru>
2022-06-07 14:26:25 +0600

    add input

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   const [todos, setTodos] = useState([])
 
   const addTask = (userInput) => {
-    if(userInput) {
+    if (userInput) {
       const newItem = {
-        id: Math.random().toString(36).substr(2,9),
+        id: Math.random().toString(36).substr(2, 9),
         task: userInput,
         complete: false
       }
@@ -22,8 +22,8 @@ function App() {
 
   const handleToggle = (id) => {
     setTodos([
-      ...todos.map((todo) => 
-        todo.id === id ? { ...todo, complete: !todo.complete } : {...todo }
+      ...todos.map((todo) =>
+        todo.id === id ? { ...todo, complete: !todo.complete } : { ...todo }
       )
     ])
   }
@@ -41,7 +41,7 @@ function App() {
             key={todo.id}
             toggleTask={handleToggle}
             removeTask={removeTask}
-            />
+          />
         )
       })}
     </div>
